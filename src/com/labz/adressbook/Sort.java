98485523fe0571e115b5a3136cdd1c6fd481cab5
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-07 20:29:36 +0530

    Refactored code and added serach the person by state and city

---
+++ src/com/labz/adressbook/Sort.java
@@ -0,0 +1,11 @@
+package com.labz.adressbook;
+
+import java.util.Comparator;
+
+public class Sort {
+	static Comparator<ContactPerson> compareCity = new Comparator<ContactPerson>() {
+        public int compare(ContactPerson one, ContactPerson two) {
+            return one.getCity().compareTo(two.getCity());
+        }
+    };
+}
